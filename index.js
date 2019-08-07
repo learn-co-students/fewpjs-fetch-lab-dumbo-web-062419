ded9b63df4378b85fc2b6e7b449bfeff5d422a47
Your Name <you@example.com>
2019-08-07 19:55:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')  // FETCHING DATA FROM API
+  .then(resp => resp.json()) // CONVERTING TO JSON FETCHED DATA
+  .then(json => renderBooks(json)) // THIS IS RUNNING THE FUNCTION ON LINE 8 RenderBooks(Json)
 
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+  const main = document.querySelector('main') // SELECTING THE WHOLE PAGE WITH MAIN 
+  json.forEach(book => {        // ITERATING THROUGH THE LIST OF BOOKS FROM API
+    const h2 = document.createElement('h2') // CREATING H2 TAG
+    h2.innerHTML = `<h2>${book.name}</h2>`    
+    main.appendChild(h2)  // ADDING h2 TO THE DOM
   })
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
